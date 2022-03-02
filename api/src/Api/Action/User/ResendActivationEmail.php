871697236bdce2988fd57b1f34219e535c1bc8cf
Dani <danielborges@live.com.ar>
2022-03-02 14:50:34 -0300

    function to resend email of activate account

---
+++ api/src/Api/Action/User/ResendActivationEmail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Api\Action\User;
+
+use App\Service\User\ResendActivationEmailService;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class ResendActivationEmail
+{
+
+    private ResendActivationEmailService $resendActivationEmail;
+
+    public function __construct(ResendActivationEmailService $resendActivationEmail)
+    {
+
+        $this->resendActivationEmail = $resendActivationEmail;
+    }
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        $this->resendActivationEmail->resend($request);
+
+        return new JsonResponse(['message' => 'Activation email sent']);
+    }
+}
